f266fecf84d766ed327a052e8afe91b78b4878f1
stopWarByWar <panyeda@ldeMacBook-Pro.local>
2021-12-30 14:00:29 +0800

    call endpoint okay

--- agent.go
+++ agent.go
@@ -33,12 +33,12 @@ func New(anonymous bool, privKey string) *Agent {
 	}
 }
 
-func (agent *Agent) Sender() *principal.Principal {
+func (agent *Agent) Sender() principal.Principal {
 	if agent.identity.Anonymous == true {
-		return &principal.AnonymousID
+		return principal.AnonymousID
 	}
 	sender := principal.NewSelfAuthenticating(agent.identity.PubKeyBytes())
-	return &sender
+	return sender
 }
 
 func (agent *Agent) getExpiryDate() time.Time {
@@ -76,7 +76,7 @@ func (agent *Agent) QueryRaw(canisterID, methodName string, arg []byte) ([]idl.T
 	}
 	req := Request{
 		Type:          RequestTypeQuery,
-		Sender:        *agent.Sender(),
+		Sender:        agent.Sender(),
 		CanisterID:    canisterIDPrincipal,
 		MethodName:    methodName,
 		Arguments:     arg,
@@ -86,6 +86,7 @@ func (agent *Agent) QueryRaw(canisterID, methodName string, arg []byte) ([]idl.T
 	if err != nil {
 		return nil, nil, "", err
 	}
+	fmt.Println("data:", hex.EncodeToString(data))
 	resp, err := agent.queryEndpoint(canisterID, data)
 	if err != nil {
 		return nil, nil, "", err
@@ -109,25 +110,28 @@ func (agent *Agent) UpdateRaw(canisterID, methodName string, arg []byte) ([]idl.
 	}
 	req := Request{
 		Type:          RequestTypeCall,
-		Sender:        *agent.Sender(),
+		Sender:        agent.Sender(),
 		CanisterID:    canisterIDPrincipal,
 		MethodName:    methodName,
 		Arguments:     arg,
-		IngressExpiry: uint64(agent.getExpiryDate().Nanosecond()),
+		IngressExpiry: uint64(agent.getExpiryDate().UnixNano()),
 	}
 
 	requestID, data, err := agent.signRequest(req)
 	if err != nil {
 		return nil, nil, err
 	}
+	fmt.Println("IngressExpiry:",req.IngressExpiry)
+	fmt.Println("update request id:", hex.EncodeToString(requestID[:]))
+	fmt.Println("data:", hex.EncodeToString(data))
+	//data,_ = hex.DecodeString("a367636f6e74656e74a66c726571756573745f747970656463616c6c6673656e646572581d8139de9ec81d50d862a956dd95e8d705e462f9e8df206ff4fe498739026b63616e69737465725f69644a0000000000f010ec01016b6d6574686f645f6e616d65687472616e73666572636172674f4449444c0002687d010080c8afa0256e696e67726573735f6578706972791b16c5481082f22e006d73656e6465725f7075626b6579582c302a300506032b6570032100ec172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf6a73656e6465725f736967584028677b532f1baaa31619923381f3a3d0be33ff4212dfc984dd3649fd0fbec938edcb30f72d59d2f01d1697a6eda3ac1c7de231974c0e3239f37ddbdf5b98f60a")
 	_, err = agent.callEndpoint(canisterID, *requestID, data)
 	if err != nil {
 		return nil, nil, err
 	}
-	fmt.Println("update request id:", hex.EncodeToString(requestID[:]))
 	//poll requestID to get result
 	//todo:这个时间写成配置之后
-	result, err := agent.poll(canisterID, *requestID, time.Second, time.Second*30)
+	result, err := agent.poll(canisterID, *requestID, time.Second, time.Second*10)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -136,7 +140,6 @@ func (agent *Agent) UpdateRaw(canisterID, methodName string, arg []byte) ([]idl.
 		return nil, nil, err
 	}
 	return types, values, nil
-
 }
 
 func (agent *Agent) poll(canisterID string, requestID RequestID, delay time.Duration, timeout time.Duration) ([]byte, error) {
@@ -144,8 +147,8 @@ func (agent *Agent) poll(canisterID string, requestID RequestID, delay time.Dura
 	var finalCert []byte
 	timer := time.NewTimer(timeout)
 	ticker := time.NewTicker(delay)
-
-	for {
+	stopped := false
+	for stopped {
 		select {
 		case <-ticker.C:
 			status, cert, err := agent.requestStatusRaw(canisterID, requestID)
@@ -155,10 +158,10 @@ func (agent *Agent) poll(canisterID string, requestID RequestID, delay time.Dura
 			finalStatus = string(status)
 			finalCert = append(finalCert, cert...)
 			if finalStatus == "replied" || finalStatus == "done" || finalStatus == "rejected" {
-				break
+				stopped = true
 			}
 		case <-timer.C:
-			break
+			stopped = true
 		}
 	}
 	if finalStatus == "replied" {
@@ -169,12 +172,14 @@ func (agent *Agent) poll(canisterID string, requestID RequestID, delay time.Dura
 		}
 		return res, nil
 	}
+	defer timer.Stop()
+	defer ticker.Stop()
 	return nil, fmt.Errorf("call poll fail with status %v", finalStatus)
 }
 
 func (agent *Agent) requestStatusRaw(canisterID string, requestId RequestID) ([]byte, []byte, error) {
 	//todo:回头看看这么编码行不行
-	paths := [][]byte{[]byte("request_status"),requestId[:]}
+	paths := [][]byte{[]byte("request_status"), requestId[:]}
 	cert, err := agent.readStateRaw(canisterID, paths)
 	if err != nil {
 		return nil, nil, err
@@ -187,14 +192,14 @@ func (agent *Agent) requestStatusRaw(canisterID string, requestId RequestID) ([]
 
 func (agent *Agent) readStateRaw(canisterID string, paths [][]byte) ([]byte, error) {
 	req := Request{
-		Type:          RequestTypeReadState,
-		Sender:        *agent.Sender(),
-		Paths:         paths,
-		//IngressExpiry: uint64(agent.getExpiryDate().Nanosecond()),
+		Type:   RequestTypeReadState,
+		Sender: agent.Sender(),
+		Paths:  paths,
+		IngressExpiry: uint64(agent.getExpiryDate().UnixNano()),
 	}
 
 	_, data, err := agent.signRequest(req)
-	fmt.Println("Data:",hex.EncodeToString(data))
+	fmt.Println("Data:", hex.EncodeToString(data))
 	//data,_ = hex.DecodeString("a367636f6e74656e74a66c726571756573745f747970656463616c6c6673656e646572581d8139de9ec81d50d862a956dd95e8d705e462f9e8df206ff4fe498739026b63616e69737465725f69644a0000000000f010ec01016b6d6574686f645f6e616d65687472616e73666572636172674f4449444c0002687d010080c8afa0256e696e67726573735f6578706972791b16c52f08c78464006d73656e6465725f7075626b6579582c302a300506032b6570032100ec172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf6a73656e6465725f73696758404093a2371ab4fc3a2e742bed6ed0606f14f35cffd40235e15ae47aa628a0375f252eaee777149d2326ec99900b135a2f2b291df0f3752223e407065947141307")
 
 	if err != nil {
@@ -212,7 +217,7 @@ func (agent *Agent) readStateRaw(canisterID string, paths [][]byte) ([]byte, err
 	if err != nil {
 		return nil, err
 	}
-	return nil,nil
+	return nil, nil
 	//return result["certificate"], nil
 }
 
@@ -229,6 +234,7 @@ func (agent *Agent) signRequest(req Request) (*RequestID, []byte, error) {
 		SenderPubkey: agent.identity.PubKeyBytes(),
 		SenderSig:    sig,
 	}
+	//fmt.Println("envelope:",envelope)
 	marshaledEnvelope, err := cbor.Marshal(envelope)
 	if err != nil {
 		return nil, nil, err
--- agent_test.go
+++ agent_test.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"encoding/hex"
+	"github.com/fxamacker/cbor/v2"
 	"github.com/stopWarByWar/ic-agent/internal/identity"
 	"github.com/stopWarByWar/ic-agent/internal/idl"
 	"github.com/stopWarByWar/ic-agent/internal/principal"
@@ -11,24 +12,36 @@ import (
 
 func TestAgent_QueryRaw(t *testing.T) {
 	canisterID := "gvbup-jyaaa-aaaah-qcdwa-cai"
-	agent := New(false, "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42")
+	agent := New(true, "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42")
 	methodName := "totalSupply"
 	arg, err := idl.Encode([]idl.Type{new(idl.Null)}, []interface{}{nil})
 	if err != nil {
 		t.Error(err)
 	}
 	_, result, errMsg, err := agent.QueryRaw(canisterID, methodName, arg)
-	t.Log("errMsg:", errMsg, "err:", err, "result:", result)
-	//t.Log("errMsg:", errMsg, "err:", err, "result:", result[0].(big.Int))
+	t.Log("errMsg:", errMsg, "err:", err, "result:", result[0].(*big.Int))
 }
 
 func TestAgent_UpdateRaw(t *testing.T) {
 	canisterID := "gvbup-jyaaa-aaaah-qcdwa-cai"
-	agent := New(false, "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42")
+	agent := New(true, "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42")
+
+	//t.Log("time:", uint64(agent.getExpiryDate().UnixNano()))
+	//envelope := new(Envelope)
+	//data, _ := hex.DecodeString("a167636f6e74656e74a66c726571756573745f747970656463616c6c6673656e64657241046e696e67726573735f6578706972791a2f449dd06b63616e69737465725f69644a0000000000f010ec01016b6d6574686f645f6e616d65687472616e73666572636172674f4449444c0002687d010080c8afa025")
+	//cbor.Unmarshal(data, envelope)
+	//t.Log("envelope ", envelope.Content)
+	//
+	//t.Log("sender", envelope.Content.Sender.Encode())
+	//t.Log("type", envelope.Content.Type)
+	//t.Log("ingress expiry", envelope.Content.IngressExpiry)
+	//t.Log("method", envelope.Content.MethodName)
+	//t.Log("arg", envelope.Content.Arguments)
+	//t.Log("canister", envelope.Content.CanisterID.Encode())
+
 	methodName := "transfer"
 	var argType []idl.Type
 	var argValue []interface{}
-
 	p, _ := principal.Decode("aaaaa-aa")
 	argType = append(argType, new(idl.Principal))
 	argType = append(argType, new(idl.Nat))
@@ -45,4 +58,35 @@ func TestPrincipal(t *testing.T) {
 	identity := identity.New(false, pkBytes)
 	p := principal.NewSelfAuthenticating(identity.PubKeyBytes())
 	t.Log(p.Encode(), len(identity.PubKeyBytes()))
+}
+
+func TestCbor(t *testing.T) {
+	canisterID, _ := principal.Decode("gvbup-jyaaa-aaaah-qcdwa-cai")
+	agent := New(true, "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42")
+
+	//t.Log("time:", uint64(agent.getExpiryDate().UnixNano()))
+	req := Request{
+		Type:          "call",
+		Sender:        agent.Sender(),
+		IngressExpiry: uint64(agent.getExpiryDate().UnixNano()),
+		CanisterID:    canisterID,
+		MethodName:    "transfer",
+		Arguments:     []byte("i love vivian"),
+	}
+
+	envelope := Envelope{
+		req,
+		[]byte{},
+		[]byte{},
+	}
+
+	data, _ := cbor.Marshal(envelope)
+	resp := new(Envelope)
+	cbor.Unmarshal(data, resp)
+	t.Log("sender", resp.Content.Sender.Encode())
+	t.Log("type", resp.Content.Type)
+	t.Log("ingress expiry", resp.Content.IngressExpiry)
+	t.Log("method", resp.Content.MethodName)
+	t.Log("arg", resp.Content.Arguments)
+	t.Log("canister", resp.Content.CanisterID.Encode())
 }
